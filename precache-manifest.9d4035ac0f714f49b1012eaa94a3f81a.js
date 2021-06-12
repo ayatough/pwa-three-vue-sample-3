6da53afc70d65f95ff4f7273c2bf8fee59f551dd
ayatough <modest.simple.life@gmail.com>
2021-06-13 07:27:36 +0900

    add dist

---
+++ precache-manifest.9d4035ac0f714f49b1012eaa94a3f81a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0099d10dfcb335a5a75",
+    "url": "/css/app.b83f57c5.css"
+  },
+  {
+    "revision": "d4cd57a01346ba4cdb57ab9fb52409ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0099d10dfcb335a5a75",
+    "url": "/js/app.b037cf03.js"
+  },
+  {
+    "revision": "6c05b8c256b95b7d19f8",
+    "url": "/js/chunk-vendors.fe4af910.js"
+  },
+  {
+    "revision": "e63673bfc88ba9a5fb9dc32acccac7d0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
